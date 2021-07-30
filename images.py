ea51ee06e8a39b2142d2e4ee2356d4e00502f606
Eric Criteser <ericcriteser@Erics-MBP.attlocal.net>
2021-07-30 14:59:20 -0500

    add preliminary output(New_York)

--- images.py
+++ images.py
@@ -1,11 +1,25 @@
 from keras.preprocessing import image
+#just kind of saving stuff
 
 #display full image with tee_ny (arbitrary) area selected
 #plt.figure(figsize = (15,15))
 #plt.gca().add_patch(Rectangle((5000, 8250), 2000, 2000, linewidth = 3, edgecolor = 'r', facecolor = 'none'));
 #plt.imshow(np.asarray(img));
 
+#full image view and full image as array
+#img = image.load_img("ny_resized.jpg")
+#full_img = image.img_to_array(img)
+
 def get_image(filename):
     return image.load_img(filename)
 
-    
+#return subarray (tee_ny) from full image, arbitrarily chosen
+#tee_ny = full_img[8250: 10250, 5000:  7000, :]
+#tee_ny_img = image.array_to_img(tee_ny)
+#plt.figure(figsize = (15,15))
+#plt.axvline(x= rand_x_start, color= 'red')
+#plt.axvline(x= rand_x_fin, color= 'red')
+#plt.axhline(y= rand_y_start, color= 'red')
+#plt.axhline(y= rand_y_fin, color= 'red')
+#plt.title('New York section with target pixel')
+#plt.imshow(tee_ny);
--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 from scipy import spatial
 
 model_width = 224
-half_model_width = int(224 * .5)
+half_model_width = int(model_width * .5)
 est_location = 200
-half_est_location = int(est_location * .5)
+#half_est_location = int(est_location * .5)
 
 #return distance between vector points
 def vector_dist(v1, v2):
@@ -20,10 +20,10 @@ def get_img_slice(t_img, x, y, model_width):
     t_array = t_img[y: y + model_width, x: x + model_width, :]
     return t_array
 
-#middle pixel in, returns starting and ending slice points
-def start_lines(pix_x, pix_y):
-    x_start = pix_x - 100 - 112 
-    x_finish = pix_x + 100 + 112
-    y_start = pix_y - 100 - 112 
-    y_finish = pix_y + 100 + 112
+#middle pixel in, returns starting point half model and half location est away from pix, same with ending points
+def start_lines(pix_x, pix_y, half_est_location):
+    x_start = pix_x - half_est_location - half_model_width 
+    x_finish = pix_x + half_est_location + half_model_width
+    y_start = pix_y - half_est_location - half_model_width 
+    y_finish = pix_y + half_est_location + half_model_width
     return x_start, x_finish, y_start, y_finish
